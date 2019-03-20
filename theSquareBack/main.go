54ed6f004448ea8721376a69ab13370e9b2afe46
Heurtault <antoine.heurtault75@protonmail.com>
2019-03-20 23:43:56 +0100

    init docker-compose

---
+++ theSquareBack/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
+}
+
+func main() {
+	fmt.Println("Serveur liste on port 8080 ..")
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+
+}
